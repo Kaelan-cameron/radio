1136862e4a493ffbe40512e9fc7882fb78285f1c
Kaelan-cameron <84246143+Kaelan-cameron@users.noreply.github.com>
2021-06-05 12:00:06 -0700

    added rotation

--- main.ts
+++ main.ts
@@ -2,9 +2,13 @@ radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber)
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0.1)
+    radio.sendValue("left", 0.1)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(3)
+    radio.sendValue("left", 0.2)
 })
 radio.setGroup(1)
+basic.forever(function () {
+    radio.sendValue("left", input.rotation(Rotation.Roll))
+    basic.pause(500)
+})
